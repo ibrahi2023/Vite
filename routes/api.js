1845f5c8e8bfbcb1908dfea257f3075b664abc70
barho <akama2253@gmail.com>
2024-11-23 21:53:48 +0100

    update

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,12 @@
 import express from 'express';
 const router = express.Router();
+import * as todoController from '../app/controllers/todoController.js';
 import * as userController from "../app/controllers/authController.js";
 import * as ClientController from "../app/controllers/ClientControler.js";
+
+router.post('/store', todoController.store);
+router.get('/show', todoController.show);
+router.delete('/destroy/:id', todoController.destroy);
 router.post("/signup",userController.signup);
 router.post("/signin",userController.signin);
 router.post('/updatesign', userController.ValideEdite);
@@ -13,4 +18,6 @@ router.post('/editClient', ClientController.EditClient);
 router.get('/GetClient', ClientController.GetClient);
 router.post('/GetMatricule', ClientController.GetMatricule);
 router.post('/GetDashClient', ClientController.GetDashClient);
+
+
 export default router;
