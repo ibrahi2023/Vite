50e75f4e0d0baf68315dce23dde90eff1e9ee558
barho <akama2253@gmail.com>
2024-11-25 22:03:12 +0100

    u

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ import mongoose from 'mongoose';
 import router from './routes/api.js';
 import rateLimit from 'express-rate-limit';
 import path from "path";
-import { fileURLToPath } from 'url';
 import helmet from 'helmet';
 import hpp from 'hpp';
 import cors from 'cors';
 import cookieParser from "cookie-parser";
 import {MAX_JSON_SIZE, MONGODB_CONNECTION, PORT, REQUEST_LIMIT_NUMBER, REQUEST_LIMIT_TIME, URL_ENCODED, WEB_CACHE} from "./app/config/config.js";
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
 const app = express();
 // middlewares
 app.use(cors());
@@ -48,9 +45,9 @@ app.use(express.static('dist'));
 
 // Serve React front end for all routes not handled by the API
 //const __dirname = path.resolve();
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+// });
 
 app.listen(PORT,()=>{
     console.log(`MERN  Server Running ${PORT}`)
