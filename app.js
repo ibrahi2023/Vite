257e77177ad15e5d82c00eac2e8425abdd1e3923
barho <akama2253@gmail.com>
2024-11-25 22:38:19 +0100

    j

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes/api.js';
 import rateLimit from 'express-rate-limit';
-import path from "path";
+//import path from "path";
 import helmet from 'helmet';
 import hpp from 'hpp';
 import cors from 'cors';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 import cookieParser from "cookie-parser";
 import {MAX_JSON_SIZE, MONGODB_CONNECTION, PORT, REQUEST_LIMIT_NUMBER, REQUEST_LIMIT_TIME, URL_ENCODED, WEB_CACHE} from "./app/config/config.js";
 const app = express();
@@ -42,7 +46,9 @@ app.use("/api", router);
 // Serve static assets for React front end
 app.use(express.static('dist'));
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+console.log(__dirname);
 // Serve React front end for all routes not handled by the API
 //const __dirname = path.resolve();
 app.get('*', (req, res) => {
